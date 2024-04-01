1cf0945e22c0ff18740aab01d152973007c39717
Barbara Araujo <barbaravivian.ti@outlook.com>
2024-03-31 21:48:54 -0300

    refactor: improving some places like api.js and index.js

--- src/App.js
+++ src/App.js
@@ -1,43 +1,54 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
 import api from './api';
-//import user from './user';
 
-class App extends Component{
-
-  state= {
-    members: [],
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      members: [],
+      loading: true,
+      error: null
+    };
   }
 
-  async componentDidMount(){
-    const response = await api.get('');
-
-    this.setState({ members: response.data});
+  async componentDidMount() {
+    try {
+      const response = await api.get('');
+      this.setState({ members: response.data, loading: false });
+    } catch (error) {
+      this.setState({ error: error.message, loading: false });
+    }
   }
 
+  render() {
+    const { members, loading, error } = this.state;
 
-  render(){
+    if (loading) {
+      return <div>Loading...</div>;
+    }
 
-    const { members }= this.state;
-    return(
+    if (error) {
+      return <div>Error: {error}</div>;
+    }
+
+    return (
       <div>
         <h1>Membros do repositório do Github da AWS</h1>
-        {console.log(members)}
-        {members.map(member => (
-          <li key={member.id}>
-
-            <h2><strong>Nome: </strong>
-            {member.name}
-            </h2>
-            <img src={member.avatar_url}/>
-            <p> Login: {member.login}</p>
-            <p></p>
-
-          </li>
-
-        ))}
+        <ul>
+          {members.map(member => (
+            <li key={member.id}>
+              <h2>
+                <strong>Nome: </strong>
+                {member.name}
+              </h2>
+              <img src={member.avatar_url} alt={`${member.name}'s avatar`} />
+              <p>Login: {member.login}</p>
+            </li>
+          ))}
+        </ul>
       </div>
     );
-  };
-};
+  }
+}
 
 export default App;
--- src/api.js
+++ src/api.js
@@ -1,8 +1,24 @@
 import axios from 'axios';
 
 const api = axios.create({
-    baseURL: 'https://api.github.com/orgs/aws/members'
-})
+  baseURL: 'https://api.github.com/orgs/aws/members'
+});
 
+api.interceptors.response.use(
+  response => {
+    return response;
+  },
+  error => {
+    if (error.response) {
+      console.error('Erro de resposta:', error.response.data);
+      console.error('Código de status:', error.response.status);
+    } else if (error.request) {
+      console.error('Sem resposta:', error.request);
+    } else {
+      console.error('Erro:', error.message);
+    }
+    return Promise.reject(error);
+  }
+);
 
 export default api;
